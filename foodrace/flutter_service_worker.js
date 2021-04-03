c4add771829e10d8890fb82136c941724a759e08
Mahady Hasan <59983619+mahadydev@users.noreply.github.com>
2021-04-03 12:14:32 +0600

    Add files via upload

--- foodrace/flutter_service_worker.js
+++ foodrace/flutter_service_worker.js
@@ -22,9 +22,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "20bdf3792dad5a450f75f92a8bb8999c",
-"/": "20bdf3792dad5a450f75f92a8bb8999c",
-"main.dart.js": "36fce9200322ac472295c34a548fa66e",
+"index.html": "07882de20f9135633f7be93982792d97",
+"/": "07882de20f9135633f7be93982792d97",
+"main.dart.js": "421b2d34e2ae0a6fd2f4273f9b6f96b6",
 "manifest.json": "ec15ada79d1d3c61f15be75b2e08ffbf",
 "version.json": "acedf6d136c2bd558848dd2ae622ec8b"
 };
